36dbc42802da832cb1fb57f075c70a3fda224332
karan-webdev <120566797+karan-webdev@users.noreply.github.com>
2023-10-29 13:48:55 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll('a');
+// service hover animation
+const serviceElements = document.querySelectorAll('.service');
 
-    for (const link of links) {
-        link.addEventListener('click', smoothScroll);
+  serviceElements.forEach((element) => {
+    element.addEventListener('mouseover', () => {
+      const randomRotation = (Math.random() * 2 - 1) * 3; // Generates a random number between -3 and 3
+      element.style.transform = `scale(1.05) rotate(${randomRotation}deg)`;
+    });
+
+    element.addEventListener('mouseout', () => {
+      element.style.transform = 'scale(1) rotate(0deg)';
+    });
+  });
+
+  
+// scroll to top button
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  window.onscroll = function() {
+    const backButton = document.getElementById("back-to-top");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      backButton.style.display = "block";
+      backButton.style.opacity = 1; 
+    } else {
+      backButton.style.opacity = 0; 
     }
-
-    function smoothScroll(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute("href").substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop,
-                behavior: "smooth" // Add smooth scrolling behavior
-            });
-        }
-    }
-});
-
+  };
 
   
   
