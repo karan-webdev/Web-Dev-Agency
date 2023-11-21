41d76738b8f4045dd91d58905a271d841c75d7d4
karan-webdev <120566797+karan-webdev@users.noreply.github.com>
2023-11-21 22:40:18 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -24,31 +24,21 @@ applyHoverAnimation(testimonialElements);
   
 
 
-  window.onscroll = function() {
-    const backButton = document.getElementById("back-to-top");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      backButton.style.display = "block";
-      backButton.style.opacity = 1; 
-    } else {
-      backButton.style.opacity = 0; 
-    }
-  };
+const cursor = document.querySelector('.cursor');
 
-  const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+})
 
-  document.addEventListener('mousemove', e => {
-      cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
-  })
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
 
-  document.addEventListener('click', () => {
-      cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
 
-      setTimeout(() => {
-          cursor.classList.remove("expand");
-      }, 500)
-  })
-  
-  document.getElementById('custom-toggler').addEventListener('click', function () {
+document.getElementById('custom-toggler').addEventListener('click', function () {
     // Toggle the 'active' class on the toggler button
     this.classList.toggle('active');
 });
